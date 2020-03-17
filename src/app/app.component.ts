3dc93eb6fef09b6170fab66c4df6495b10cdea97
fermeaux.sfeir <fermeaux.t@sfeir.com>
2020-03-17 17:09:08 +0100

    feat(sqlite): met en place une démo utilisant sqlite dans angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Item } from "../assets/model/item.schema";
+import { OnInit, Component } from "@angular/core";
+import { AppService } from "./app.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "App",
+  template: `
+    <div style="text-align:center">
+      <h1>Welcome to {{ title }}!</h1>
+      <button (click)="addItem()" mat-raised-button>Add Item</button>
+      <button (click)="deleteItem()" mat-raised-button>Delete Item</button>
+      <h2>Here is the contents of the database:</h2>
+      <div>
+        <ul style="list-style: none">
+          <li *ngFor="let item of itemList">
+            {{ item.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'angularElectronDemo';
+export class AppComponent implements OnInit {
+  public readonly title = "my app";
+  itemList: Item[];
+
+  constructor(private appservice: AppService) {}
+
+  ngOnInit(): void {
+    console.log("component initialized");
+    this.appservice.getItems().subscribe(items => (this.itemList = items));
+  }
+
+  addItem(): void {
+    let item = new Item();
+    item.name = "Item " + this.itemList.length;
+    this.appservice.addItem(item).subscribe(items => (this.itemList = items));
+  }
+
+  deleteItem(): void {
+    const item = this.itemList[this.itemList.length - 1];
+    this.appservice
+      .deleteItem(item)
+      .subscribe(items => (this.itemList = items));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { AppService } from "./app.service";
+import { ElectronService } from "ngx-electron";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [AppService, ElectronService]
 })
-export class AppModule { }
+export class AppModule {}
