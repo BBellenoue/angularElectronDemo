57e16b4667b0d92581c0b15f151e0b55298d1a94
fermeaux.sfeir <fermeaux.t@sfeir.com>
2020-03-18 16:05:36 +0100

    feat(sqlite): implémente sqlite

--- index.prod.ts
+++ index.prod.ts
@@ -1,12 +1,31 @@
-const { app, BrowserWindow } = require("electron");
+import "reflect-metadata";
+
+import { app, BrowserWindow, ipcMain } from "electron";
+
+import { createConnection } from "typeorm";
+
+import { Item } from "./src/assets/model/item.schema";
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let mainWindow: Electron.BrowserWindow | null;
 
-function createWindow() {
+const isDevMode = process.execPath.match(/[\\/]electron/);
+
+const createWindow = async () => {
+  const connection = await createConnection({
+    type: "sqlite",
+    synchronize: true,
+    logging: true,
+    logger: "simple-console",
+    database: "./src/assets/data/database.sqlite",
+    entities: [Item]
+  });
+
+  const itemRepo = connection.getRepository(Item);
+
   // Create the browser window.
-  win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -15,17 +34,50 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  //win.loadFile('dist/angularElectronDemo/index.html')
-  win.loadFile("dist/angularElectronDemo/index.html");
+  mainWindow.loadFile("dist/angularElectronDemo/index.html");
+  // mainWindow.loadURL("http://localhost:4200");
+
+  // Open the DevTools.
+  if (isDevMode) {
+    mainWindow.webContents.openDevTools();
+  }
 
   // Emitted when the window is closed.
-  win.on("closed", () => {
+  mainWindow.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
+    mainWindow = null;
   });
-}
+
+  ipcMain.on("get-items", async (event: any, ...args: any[]) => {
+    try {
+      event.returnValue = await itemRepo.find();
+    } catch (err) {
+      throw err;
+    }
+  });
+
+  ipcMain.on("add-item", async (event: any, _item: Item) => {
+    try {
+      const item = await itemRepo.create(_item);
+      await itemRepo.save(item);
+      event.returnValue = await itemRepo.find();
+    } catch (err) {
+      throw err;
+    }
+  });
+
+  ipcMain.on("delete-item", async (event: any, _item: Item) => {
+    try {
+      const item = await itemRepo.create(_item);
+      await itemRepo.remove(item);
+      event.returnValue = await itemRepo.find();
+    } catch (err) {
+      throw err;
+    }
+  });
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -34,7 +86,7 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
+  // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
@@ -42,12 +94,12 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (mainWindow === null) {
     createWindow();
   }
 });
 
 // In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// code. You can also put them in separate files and import them here.
